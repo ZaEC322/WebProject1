c86ac74e9215e7c39e75bbc3c588f214ec38bbaa
artem4090@gmail.com <artem4090@gmail.com>
2021-01-22 19:49:14 +0200

    получилось, кнопка таки заработаа

--- WebProject1/module1.py
+++ WebProject1/module1.py
@@ -1,11 +1,29 @@
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import sys
 
-Form, Window = uic.loadUiType("untitled.ui")
 
-app = QApplication([])
-window = Window()
-form = Form()
-form.setupUi(window)
-window.show()
-app.exec_()
+def clicked():
+    print("cleck")
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,300,300)
+    win.setWindowTitle("asd")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("addsad")
+    label.move(50,50)
+
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("click me")
+    b1.clicked.connect(clicked)
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
+
+
+
